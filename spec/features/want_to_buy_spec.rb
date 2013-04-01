e4ab5095e75544f4b67a652158977a6fba980ba3
mercury <mercury@mercury-compaq.(none)>
2013-04-01 21:43:28 +0800

    fixing creating new buy request

--- spec/features/want_to_buy_spec.rb
+++ spec/features/want_to_buy_spec.rb
@@ -6,11 +6,6 @@ feature "[Want to buy]" do
   end
 
   scenario "Creating a new buy request" do
-    pending "Read The Following rails cast to do this correctly \n \
-             http://railscasts.com/episodes/346-wizard-forms-with-wicked \n \
-             http://railscasts.com/episodes/253-carrierwave-file-uploads \n \
-             http://railscasts.com/episodes/381-jquery-file-upload \n \
-             then remove this pending line from this test"
     visit "/wtb/new"
     fill_in "Item", with: "Playstation controller"
     fill_in "Budget", with: "RM 300.00"
